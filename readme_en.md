9bc291eae6564cfe6ea9140d3b782eb5390cb4af
dd <dyc87112@qq.com>
2023-03-28 20:12:43 +0800

    up

---
+++ readme_en.md
@@ -0,0 +1,6 @@
+# 使用说明
+
+1. 通过阿里云函数计算部署
+2. 绑定自定义域名，比如：http://openai-proxy.abc123.com
+3. 在后续需要调用 https://api.openapi.com 的地方使用上面自定义的域名
+
---
+++ src/code/app.js
@@ -0,0 +1,18 @@
+  const express = require('express')
+  const {
+    createProxyMiddleware
+  } = require('http-proxy-middleware');
+  const app = express()
+  const port = 9000
+
+  app.use('/', createProxyMiddleware({
+    target: 'https://api.openai.com',
+    changeOrigin: true,
+    onProxyRes: function (proxyRes, req, res) {
+      proxyRes.headers['Access-Control-Allow-Origin'] = '*';
+    }
+  }));
+
+  app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+  })
